1d99f7ad405baf9ec4c72c2476356374db918a51
Ian Monroe <ian@ianmonroe.com>
2023-10-17 18:46:29 -0600

    tinker with auth for api access

--- docroot/sites/settings/saml.settings.php
+++ docroot/sites/settings/saml.settings.php
@@ -15,12 +15,10 @@ $config['stanford_ssp.settings'] = [
 ];
 
 // Always show local login when not on Acquia.
-//if (!EnvironmentDetector::isAhEnv()) {
-
-// CASBS specific - they use basic_auth for API access
+if (!EnvironmentDetector::isAhEnv()) {
   $config['stanford_ssp.settings']['hide_local_login'] = FALSE;
   $config['stanford_samlauth.settings']['hide_local_login'] = FALSE;
-//}
+}
 
 // Remove this when simplesamlphp is removed.
 $config['simplesamlphp_auth.settings'] = [
